e9330ccc4f9bec951da50c7da83d5154bb282e32
Tailszefox <tails@tailszefox.net>
2019-01-28 06:30:07 +0100

    timelapse: changed conditions for new session

--- timelapse.sh
+++ timelapse.sh
@@ -8,41 +8,40 @@
 
 cd ~/timelapse
 
-date
-
 # The recorded date from the last time the script was called
-recordedDate=`cat date`
+lastTimestamp=`cat lastTimestamp`
 
-currentDate=`date +"%d"`
+currentLocalDate=`date`
+currentFullDate=`date +"%F"`
+currentDay=`date +"%d"`
 currentMinute=`date +"%-M"`
 currentHour=`date +"%-H"`
-currentFullDate=`date +"%F"`
+currentTimestamp=`date +"%s"`
 
 # The maximum system load allowed for making a video
 maxLoad=1.00
 
-# From midnight to 6 AM, continue working on yesterday's video rather than making a new one
-if [[ $currentHour -lt 6 ]]; then
-    currentDate=$recordedDate
-    currentFullDate=`date +"%F" -d "yesterday"`
-fi
+# Check last time the script was called
+secondsSinceLast=$(($currentTimestamp-$lastTimestamp))
 
-# The script is called for the first time after 6 AM
-if [[ ${recordedDate#0} -ne ${currentDate#0} ]]; then
+# If last call was more than an hour ago, start a new session
+if [[ $secondsSinceLast -gt 3600 ]]; then
     echo "" > ./log
+    echo "Starting a new session"
 
-    # Delete archived screenshots
-    find ./yesterday -maxdepth 1 -name "*.jpg" -delete
+    # Delete oldest screenshots
+    find ./previous/3 -maxdepth 1 -name "*.jpg" -delete
 
-    # Archive current screenshots
-    find -maxdepth 1 -name "*.jpg" -execdir mv '{}' ./yesterday \;
-
-    # Delete old videos
-    find ./videos -maxdepth 1 -name "*.mp4" -ctime +7 -delete
-
-    echo $currentDate > ./date
-    exit
+    # Shift screenshots to previous day
+    find ./previous/2 -maxdepth 1 -name "*.jpg" -execdir mv '{}' ../3  \;
+    find ./previous/1 -maxdepth 1 -name "*.jpg" -execdir mv '{}' ../2  \;
+    find ./ -maxdepth 1 -name "*.jpg" -execdir mv '{}' ./previous/1  \;
 fi
+
+echo "$currentLocalDate / ($currentTimestamp - $lastTimestamp) = $secondsSinceLast"
+
+# Save new timestamp
+echo $currentTimestamp > ./lastTimestamp
 
 # Get how long the user has been idle
 idle=`DISPLAY=:0 /usr/local/bin/xprintidle-ng`
@@ -59,21 +58,21 @@ if (( $idleMinute >= 15 )); then
     fi
 fi
 
-DISPLAY=:0 scrot -q 100 "$currentDate-%s.jpg"
+DISPLAY=:0 scrot -q 100 "%s.jpg"
 
 # Every half hour, check to make the video
-if (( $currentMinute % 30 == 0 )); then
-    echo "Checking to make video..."
-    load=`cat /proc/loadavg | cut -d ' ' -f 2`
-    echo "Current load: $load"
+# if (( $currentMinute % 30 == 0 )); then
+#     echo "Checking to make video..."
+#     load=`cat /proc/loadavg | cut -d ' ' -f 2`
+#     echo "Current load: $load"
 
-    # If the system is overloaded, don't try making the video
-    if (( $(echo "$load > $maxLoad " |bc -l) )); then
-        echo "$load > $maxLoad, not making video"
-        exit
-    fi
+#     # If the system is overloaded, don't try making the video
+#     if (( $(echo "$load > $maxLoad " |bc -l) )); then
+#         echo "$load > $maxLoad, not making video"
+#         exit
+#     fi
 
-    # Start ffmpeg with nice and 1 thread so it takes the least amount of resources possible
-    nice -n 19 ffmpeg -framerate 3 -pattern_type glob -i "$currentDate-*.jpg" -c:v libx264 -pix_fmt yuv420p -r 6 -y -threads 1 videos/video-$currentFullDate.mp4
-    echo "Video done"
-fi
+#     # Start ffmpeg with nice and 1 thread so it takes the least amount of resources possible
+#     nice -n 19 ffmpeg -framerate 3 -pattern_type glob -i "$currentDate-*.jpg" -c:v libx264 -pix_fmt yuv420p -r 6 -y -threads 1 videos/video-$currentFullDate.mp4
+#     echo "Video done"
+# fi
