0e0e9160b6fe36fec04bcdeecabb01d02ab6abc6
Tailszefox <tails@tailszefox.net>
2019-06-05 03:46:11 +0200

    recreate.sh: clearer output

--- recreate.sh
+++ recreate.sh
@@ -2,14 +2,16 @@
 
 # Recreate hard links if the file exists both in the current directory and in the ~/bin directory
 
-find . -maxdepth 1 -type f -print0 | while IFS= read -r -d $'\0' f; do
-    echo "Analyzing $f"
-    ls -l ~/bin/$f
+find . -maxdepth 1 -type f | while read f; do
+    filename=$(basename $f)
+    echo "Analyzing $filename"
 
-    if [[ -f ~/bin/$f ]]; then
-        echo "Recreating symlink: ~/bin/$f -> ./$f"
+    if [[ -f ~/bin/$filename ]]; then
+        echo "Recreating hard link: ~/bin/$filename -> ./$filename"
         rm "$f"
-        ln ~/bin/$f .
+        ln ~/bin/$filename .
+    else
+        echo "$filename not found in ~/bin"
     fi
 
     echo
