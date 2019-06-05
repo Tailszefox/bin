ed7b046253c0de2952ee605c9f5a763bccd8a6ab
Tailszefox <tails@tailszefox.net>
2019-06-05 03:45:47 +0200

    recreate.sh: English messages

--- recreate.sh
+++ recreate.sh
@@ -3,11 +3,11 @@
 # Recreate hard links if the file exists both in the current directory and in the ~/bin directory
 
 find . -maxdepth 1 -type f -print0 | while IFS= read -r -d $'\0' f; do
-    echo "Analyse de $f"
+    echo "Analyzing $f"
     ls -l ~/bin/$f
 
     if [[ -f ~/bin/$f ]]; then
-        echo "Recréation du lien"
+        echo "Recreating symlink: ~/bin/$f -> ./$f"
         rm "$f"
         ln ~/bin/$f .
     fi
